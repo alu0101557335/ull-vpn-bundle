c6b9a0c52fbd111f09e88aba8bc040b61662854b
Pablo Domenico Goya Amato <118691444+alu0101557335@users.noreply.github.com>
2023-09-14 15:00:26 +0100

    Update install-vpn.sh
    Added an upgrade command for the packages for the installation file.

--- install-vpn.sh
+++ install-vpn.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/bash
-sudo apt-get -y update
+sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt install -y network-manager-vpnc-gnome
 sudo cat > /etc/vpnc/ull.conf << EOF
 IPSec gateway vpn.ull.es
 IPSec ID ULL
 IPSec secret usu4r10s
 EOF
-
